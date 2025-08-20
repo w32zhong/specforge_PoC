d99baf83d58b2ae7718d77b9240b38f9442fdaa2
w32zhong <clock126@126.com>
2025-08-20 18:05:23 +0000

    + docker files/utils.

---
+++ docker_utils.sh
@@ -0,0 +1,28 @@
+CR_PREFIX=${1-ghcr.io/w32zhong}
+
+function build() {
+    module=$1
+    set -x
+    docker build -f ${module}_dockerfile --build-arg MODEL=$module -t $module .
+    docker image tag $module ${CR_PREFIX}/${module}:latest
+    set +x
+}
+
+function push() {
+    module=$1
+    set -x
+    docker push ${CR_PREFIX}/${module}:latest
+    set +x
+}
+
+function build_and_push() {
+    module=$1
+    build $module
+    push $module
+}
+
+function cleanup() {
+    docker container prune -f
+    docker image prune -f
+    docker images
+}
