f33729e225f03f743835be62694c85935457ed43
w32zhong <clock126@126.com>
2025-08-20 06:17:51 +0000

    fix typo.

--- specforge_het/train.py
+++ specforge_het/train.py
@@ -302,7 +302,7 @@ def main(config_file='configs.ini', **injects):
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
 
-    # use a more input-insentive scaled-dot-product kernel?
+    # use an input-insensitive scaled-dot-product kernel?
     torch.backends.cuda.enable_cudnn_sdp(False)
     torch.backends.cuda.sdp_kernel(
         enable_flash=True, enable_mem_efficient=True, enable_math=True
