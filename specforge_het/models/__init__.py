2b17f234018e314f43b128b56f6aeeff8cb3fe97
w32zhong <clock126@126.com>
2025-08-20 17:17:57 +0000

    + qwen3 models.

--- specforge_het/models/__init__.py
+++ specforge_het/models/__init__.py
@@ -1,3 +1,4 @@
 from .algorithms.eagle_v2 import EagleV2
 from .speculative_llama.modeling_speculative_llama import SpeculativeLlamaForCausalLM, LlamaDrafter
+from .speculative_qwen3.modeling_speculative_qwen3 import SpeculativeQwen3ForCausalLM, Qwen3Drafter
 from .speculative_qwen3_moe.modeling_speculative_qwen3_moe import SpeculativeQwen3MoeForCausalLM, Qwen3MoeDrafter
