6363046b520c35dbe14960f39299d6e082cfa96b
w32zhong <clock126@126.com>
2025-09-06 00:48:10 -0400

    update demo.py

--- demo.py
+++ demo.py
@@ -12,19 +12,23 @@ if not os.path.exists('./output/temp_save'):
     model = SpeculativeQwen3MoeForCausalLM.from_basemodel(
         base_config, base_model_path,
         AlgoClass=EagleV2, algo_kwargs=algo_kwargs,
-        revision=revision, _fast_init=False, device_map="auto",
+        revision=revision, device_map="auto",
         torch_dtype=torch.bfloat16
     )
     draft_config_path = 'meta-llama/Llama-2-7b-chat-hf'
     draft_config = AutoConfig.from_pretrained(draft_config_path)
     draft_model = LlamaDrafter(draft_config, model)
     model.set_draft_model(draft_model)
+
+    # (training here ...)
+
     model.save_pretrained('./output/temp_save')
 
 else:
     model = AutoModelForCausalLM.from_pretrained('./output/temp_save',
-        device_map="auto", revision=revision,
-        torch_dtype=torch.bfloat16, trust_remote_code=True
+        revision=revision, device_map="auto",
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True # needs downloading the base model
     )
 
 print(model)
