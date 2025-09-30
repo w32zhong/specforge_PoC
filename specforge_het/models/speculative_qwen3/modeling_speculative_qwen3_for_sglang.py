361044ffee843d7ed77bca62b22b5ec03dc7c718
w32zhong <clock126@126.com>
2025-09-30 15:59:23 -0400

    Qwen3DrafterForSGLang is working.

--- specforge_het/models/speculative_qwen3/modeling_speculative_qwen3_for_sglang.py
+++ specforge_het/models/speculative_qwen3/modeling_speculative_qwen3_for_sglang.py
@@ -1,10 +1,48 @@
 import json
 import torch
+from functools import partial
 from colorama import Fore, Style
 from specforge_het.models import *
 from sglang.srt.layers.quantization.base_config import QuantizationConfig # resolve circ dep
 from sglang.srt.models.qwen3 import *
 from sglang.srt.utils import add_prefix
+
+
+class ReturnTupleIdentity(torch.nn.Identity):
+    def forward(self, *args, **kwargs):
+        input = args[0] if args else next(iter(kwargs.values()))
+        return (input, None)
+
+
+def EagleV2_adapt(self, base_model_config, draft_model_config):
+    if base_model_config.get('skip_first_input_layernorm', True):
+        del self.model.layers[0].input_layernorm
+    if base_model_config.get('skip_output_norm', True):
+        self.model.norm = ReturnTupleIdentity()
+
+    # Embedding layers are handled in SGLang Worker.set_embed_and_head().
+    # Under tie_word_embeddings, this could be deleted twice by the SGLang
+    # speculative decoding worker. So let's create a placeholder here.
+    if draft_model_config.tie_word_embeddings:
+        assert id(self.lm_head.weight) == id(self.model.embed_tokens.weight)
+        self.lm_head = torch.nn.Linear(1,1)
+
+    def fwd_hook(self, input_ids, positions, forward_batch, input_embeds, **kwargs):
+        assert self.pp_group.is_first_rank
+        origin_forward = kwargs.pop('origin_forward')
+        if input_embeds is None:
+            hidden_states = self.embed_tokens(input_ids)
+        else:
+            hidden_states = input_embeds
+        prev_states = forward_batch.spec_info.hidden_states
+        hidden_states = self.eagle_fc(
+            torch.cat((hidden_states, prev_states), dim=-1)
+        )
+        return origin_forward(input_ids, positions, forward_batch,
+                              hidden_states, kwargs['pp_proxy_tensors'])
+    self.model.forward = partial(fwd_hook, self.model,
+                                 origin_forward=self.model.forward)
+
 
 
 class Qwen3DrafterForSGLang(Qwen3ForCausalLM):
@@ -14,29 +52,21 @@ class Qwen3DrafterForSGLang(Qwen3ForCausalLM):
         prefix: str = ""):
 
         # move and store base model config
-        self._base_model_config_ = json.loads(config._base_model_config)
+        base_model_config = json.loads(config._base_model_config)
         delattr(config, '_base_model_config')
 
         # call default SGLang constructor
         super().__init__(config, quant_config, prefix)
 
-        self.model.layers[0].input_layernorm = torch.nn.Identity()
-        self.model.norm = torch.nn.Identity()
-        del self.lm_head
-        del self.model.embed_tokens
-
         # bind the algorithm
-        speculative_algorithm = self._base_model_config_['speculative_decoding_algorithm']
+        speculative_algorithm = base_model_config['speculative_decoding_algorithm']
         AlgoClass = eval(speculative_algorithm)
         AlgoClass.bind_model(self, load_device=None)
 
-    # adaptor methods for EagleV2
-    @property
-    def draft_model(self):
-        return self.model
-
-    def get_hidden_size(self):
-        return self.config.hidden_size
+        if AlgoClass.__name__ == 'EagleV2':
+            EagleV2_adapt(self, base_model_config, config)
+        else:
+            raise NotImplementedError
 
     # overriding SGLang methods
     def load_weights(self, weights):
@@ -59,3 +89,11 @@ class Qwen3DrafterForSGLang(Qwen3ForCausalLM):
             #import rpdb; rpdb.set_trace()
 
         super().load_weights(weights)
+
+    # speculative decoding algorithm interfaces
+    @property
+    def draft_model(self):
+        return self.model
+
+    def get_hidden_size(self):
+        return self.config.hidden_size
