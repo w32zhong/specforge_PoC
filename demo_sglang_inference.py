c050ea93b28a4fb54a13c5b74637c9fa590bbe93
w32zhong <clock126@126.com>
2025-10-07 18:18:42 +0000

    default max_new_tokens to None

--- demo_sglang_inference.py
+++ demo_sglang_inference.py
@@ -242,7 +242,7 @@ def run_mtbench(llm, questions, sampling_params, num_threads):
 
 
 def engine_mode(model_path, draft_model=None, dtype='auto', bs=1, tp_size=1,
-    disable_cuda_graph=False, disable_radix_cache=True, max_new_tokens=4096,
+    disable_cuda_graph=False, disable_radix_cache=True, max_new_tokens=None,
     temperature=0, speculative_algorithm=None, speculative_tree=(6, 10, 60),
     mtbench=None, outfile=None, log_level="INFO", one_example_warmup=True):
 
